d9f28e997340dbc459cb5394f7227e90191e3386
Thornton <thorfernbacher@gmail.com>
2019-01-23 17:06:44 -0500

    init commit

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	public int height, width;
+	GameObject tile;
+	public void Gen() {
+		Transform t = gameObject.transform.Find("Tiles");
+		if(t != null) {
+			Destroy(t.gameObject);
+		}
+		GameObject tiles = new GameObject("Tiles");
+		tiles.transform.parent = gameObject.transform;
+		for(int x = 0; x < width; x++) {
+			for(int y = 0; y < height; y++) {
+				Instantiate(tile, tiles, new Vector3(x, y, 0));
+			}
+		}
+		Debug.Log("Generate");
+	}
+	public void Load() {
+		tile = Resources.Load<GameObject>("tile");
+	}
+}
---
+++ Assets/LevelGeneratorEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(LevelGenerator))]
+public class LevelGeneratorEditor : Editor {
+
+	LevelGenerator generator;
+	void OnEnable()
+    {
+		generator = (target as LevelGenerator);
+		generator.Load();
+    }
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        if(GUILayout.Button("Generate"))
+        {
+			generator.Gen();
+        }
+    }
+
+}
