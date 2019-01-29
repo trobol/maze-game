f548296e821b1e9f948c53c31f2a1bc1777ebf09
thorfernbacher <thorfernbacher@gmail.com>
2019-01-29 00:33:52 -0500

    lines v0.1

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -2,39 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelGenerator : MonoBehaviour {
+public class LevelGenerator : MonoBehaviour
+{
 
 	public int height, width;
-    public int lineCount;
-    public Vector2 range;
-    public Line[] lines = { };
+	public int lineCount;
+	public Vector2 range;
+	public int extra = 1;
+	public Line[] lines = { };
 	GameObject tile;
-    public void Gen() {
-        Transform t = transform.Find("Tiles");
-        if (t != null) {
-            DestroyImmediate(t.gameObject);
-        }
-        /*
-        GameObject tiles = new GameObject("Tiles");
-        tiles.transform.parent = gameObject.transform;
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                Instantiate(tile, new Vector3(x, y, 0), Quaternion.identity, tiles.transform);
-            }
-        }
-        Debug.Log("Generate");
-        */
-        lines = new Line[lineCount];
-        Vector2 p0 = new Vector2(Random.Range(range.x, range.y), Random.Range(range.x, range.y)), p1;
-        for (int i = 0; i < lineCount; i++)
-        {
-            p1 = new Vector2(Random.Range(range.x, range.y), Random.Range(range.x, range.y));
-            lines[i] = new Line(p0, p1);
-            p0 = p1;
-        }
-        print(lines);
+
+	public float tilesize = 1;
+	public void Gen()
+	{
+		Transform t = transform.Find("Tiles");
+		if (t != null)
+		{
+			DestroyImmediate(t.gameObject);
+		}
+
+		GameObject tiles = new GameObject("Tiles");
+		tiles.transform.parent = gameObject.transform;
+		Debug.Log("Generate");
+
+		lines = new Line[lineCount + extra];
+		Vector2 start = new Vector2(0, 0),
+		p0 = new Vector2(Random.Range(range.x, range.y), Random.Range(range.x, range.y)),
+		p1;
+
+		for (int i = 0; i < lineCount; i++)
+		{
+			p1 = new Vector2(p0.x + Random.Range(range.x, range.y), p0.y + Random.Range(range.x, range.y));
+			lines[i] = new Line(p0, p1);
+			p0 = p1;
+		}
+		for (int i = 0; i < extra; i++)
+		{
+			p0 = lines[Random.Range(0, lineCount - 1)].p0;
+			p1 = lines[Random.Range(0, lineCount - 1)].p0;
+			lines[lineCount + i] = new Line(p0, p1);
+		}
+
+		for (int l = 0; l < lines.Length; l++)
+		{
+			Line line = lines[l];
+			Vector2 larger, smaller, dif, tilePos;
+
+			if (line.p0.x > line.p1.x)
+			{
+				larger = line.p0;
+				smaller = line.p1;
+			}
+			else
+			{
+				larger = line.p1;
+				smaller = line.p0;
+			}
+			dif = larger - smaller;
+			for (int x = 0; x < (int)dif.x; x++)
+			{
+				tilePos = line.GetPoint(x / dif.x);
+				Instantiate(tile, new Vector3(tilePos.x, tilePos.y, 0), Quaternion.identity, tiles.transform);
+			}
+		}
+
 	}
-	public void Load() {
+	public void Load()
+	{
 		tile = Resources.Load<GameObject>("tile");
 	}
+
 }
--- Assets/Line.cs
+++ Assets/Line.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Line
 {
-    public Vector2 p0, p1;
-    public Line(Vector2 i0, Vector2 i1)
-    {
-        p0 = i0;
-        p1 = i1;
-    }
-    public Line()
-    {
-        p0 = Vector2.zero;
-        p1 = Vector2.zero;
-    }
+	public Vector2 p0, p1;
+	public Line(Vector2 i0, Vector2 i1)
+	{
+		p0 = i0;
+		p1 = i1;
+	}
+	public Line()
+	{
+		p0 = Vector2.zero;
+		p1 = Vector2.zero;
+	}
+
+	public Vector2 GetPoint(float a)
+	{
+
+		Vector2 larger, smaller, dif, tilePos;
+
+		if (p0.x > p1.x)
+		{
+			larger = p0;
+			smaller = p1;
+		}
+		else
+		{
+			larger = p1;
+			smaller = p0;
+		}
+		dif = larger - smaller;
+		return smaller + (dif) * a;
+	}
 }
